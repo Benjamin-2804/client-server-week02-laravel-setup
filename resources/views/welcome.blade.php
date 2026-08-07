60474e8265cf813a3ee8a8d3fce0ea91a1ae6e16
Benjamin <bmikr146@gmail.com>
2026-08-07 11:30:16 +0800

    Initial commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Homepage</title>
+</head>
+<body>
+
+    <h1>My Student Homepage</h1>
+
+    <ul>
+        <li><strong>Student Name:</strong> Benjamin Isidro Abadier Jr.</li>
+        <li><strong>Student Number:</strong> 123-0036</li>
+        <li><strong>Course:</strong> Bachelor of Science in Information Technology</li>
+        <li><strong>Section:</strong> 3B</li>
+        <li><strong>Subject:</strong> Client-Server Technologies</li>
+        <li><strong>Current Date:</strong> {{ date('Y-m-d') }}</li>
+    </ul>
+
+</body>
+</html>
